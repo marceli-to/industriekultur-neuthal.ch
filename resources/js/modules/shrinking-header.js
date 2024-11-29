791e2494eab99635189db517e204ef603b02a945
Marcel Stadelmann <m@marceli.to>
2024-11-29 08:28:14 +0100

    wip

--- resources/js/modules/shrinking-header.js
+++ resources/js/modules/shrinking-header.js
@@ -2,15 +2,25 @@
 
   /* Selectors */
   const header = document.querySelector('[data-header]');
+  
+  /* State */
+  let lastScrollY = window.scrollY;
+  const threshold = 250;
 
   /* Methods */
   const shrink = () => {
     if (!header) return;
-    if (window.scrollY > header.offsetHeight) {
+    
+    const currentScrollY = window.scrollY;
+    const scrollDistance = currentScrollY - lastScrollY;
+
+    if (scrollDistance > threshold) {
       header.classList.add('is-shrinked');
+      lastScrollY = currentScrollY;
     }
-    else if (window.scrollY < header.offsetHeight) {
+    else if (scrollDistance < -threshold) {
       header.classList.remove('is-shrinked');
+      lastScrollY = currentScrollY;
     }
   };
 
