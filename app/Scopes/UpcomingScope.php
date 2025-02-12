fffb161aa877c12c3392d9f6b7637d3886f3ec54
Marcel Stadelmann <m@marceli.to>
2025-02-12 10:15:51 +0100

    wip

---
+++ app/Scopes/UpcomingScope.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Scopes;
+
+use Statamic\Query\Scopes\Scope;
+use Carbon\Carbon;
+
+class UpcomingScope extends Scope
+{
+  /**
+   * Apply the scope.
+   *
+   * @param \Statamic\Query\Builder $query
+   * @param array $values
+   * @return void
+   */
+  public function apply($query, $values)
+  {
+    $query->where('event_date', '>=', Carbon::today()->toDateString());
+  }
+}
+
